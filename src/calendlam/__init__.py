222625a151694adba63147e22c7f861c6aef71d1
Jakub Stribrny <jakub.stribrny@datasentics.com>
2025-12-29 20:20:44 +0100

    defined functions

--- src/calendlam/__init__.py
+++ src/calendlam/__init__.py
@@ -2,45 +2,36 @@ from jinja2 import Environment, FileSystemLoader
 from weasyprint import HTML
 import datetime as dt
 
-# init dates
-year_start = dt.date(2026, 1, 1)
-year_end = dt.date(2026, 12, 31)
-
-
-# define day and month names
-days_en = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
-days_cz = ["pondělí", "úterý", "středa", "čtvrtek", "pátek", "sobota", "neděle"]
-months_en = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
-months_cz = ["leden", "únor", "březen", "duben", "květen", "červen", "červenec", "srpen", "září", "říjen", "listopad", "prosinec"]
-
-
 # generate data structure for year
-months = []
-current_date = year_start
-while(current_date <= year_end):
-    month = {
-        "name_cz": months_cz[current_date.month - 1],
-        "name_en": months_en[current_date.month - 1],
-        "weeks": []
-    }
+def generate_data_structure_for_full_year(months_cz, months_en, days_cz, days_en):
+    months = []
+    current_date = year_start
+    while(current_date <= year_end):
+        month = {
+            "name_cz": months_cz[current_date.month - 1],
+            "name_en": months_en[current_date.month - 1],
+            "weeks": []
+        }
 
-    this_month = current_date.month
-    while(current_date.month == this_month and current_date <= year_end):
-        week = []
-        for _ in range(7):
-            week.append({
-                "date": current_date,
-                "day_number": current_date.day,
-                "day_cz": days_cz[current_date.weekday()],
-                "day_en": days_en[current_date.weekday()],
-            })
-            current_date += dt.timedelta(days=1)
-            if(current_date.month != this_month or current_date == year_end):
-                break
+        this_month = current_date.month
+        while(current_date.month == this_month and current_date <= year_end):
+            week = []
+            for _ in range(7):
+                week.append({
+                    "date": current_date,
+                    "day_number": current_date.day,
+                    "day_cz": days_cz[current_date.weekday()],
+                    "day_en": days_en[current_date.weekday()],
+                })
+                current_date += dt.timedelta(days=1)
+                if(current_date.month != this_month or current_date == year_end):
+                    break
 
-        month["weeks"].append(week)
+            month["weeks"].append(week)
 
-    months.append(month)
+        months.append(month)
+
+        return months
 
 
 def print_and_dump(months):
@@ -53,8 +44,26 @@ def print_and_dump(months):
 #print_and_dump(months)
 
 # render and print pages
-jinja_env = Environment(loader=FileSystemLoader("templates"))
-template = jinja_env.get_template("week.jinja")
+def load_template(template_name):
+    jinja_env = Environment(loader=FileSystemLoader("templates"))
+    template = jinja_env.get_template("week.jinja")
+
+    return template
+
+# init dates
+year_start = dt.date(2026, 1, 1)
+year_end = dt.date(2026, 12, 31)
+
+
+# define day and month names
+days_en = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
+days_cz = ["pondělí", "úterý", "středa", "čtvrtek", "pátek", "sobota", "neděle"]
+months_en = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+months_cz = ["leden", "únor", "březen", "duben", "květen", "červen", "červenec", "srpen", "září", "říjen", "listopad", "prosinec"]
+
+
+months = generate_data_structure_for_full_year(months_cz, months_en, days_cz, days_en)
+template = load_template("week.jinja")
 for i, month in enumerate(months):
     for j, page in enumerate(month["weeks"]):
         print(page)
