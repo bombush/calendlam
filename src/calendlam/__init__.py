434dda040a23c712a611aa569076b718c6f8ccd2
Jakub Stribrny <jakub.stribrny@datasentics.com>
2025-12-29 21:18:31 +0100

    render all in one page

--- src/calendlam/__init__.py
+++ src/calendlam/__init__.py
@@ -1,6 +1,7 @@
 from jinja2 import Environment, FileSystemLoader
 from weasyprint import HTML
 import datetime as dt
+import os
 
 # generate data structure for year
 def generate_data_structure_for_full_year(months_cz, months_en, days_cz, days_en):
@@ -31,7 +32,7 @@ def generate_data_structure_for_full_year(months_cz, months_en, days_cz, days_en
 
         months.append(month)
 
-        return months
+    return months
 
 
 def print_and_dump(months):
@@ -46,9 +47,58 @@ def print_and_dump(months):
 # render and print pages
 def load_template(template_name):
     jinja_env = Environment(loader=FileSystemLoader("templates"))
-    template = jinja_env.get_template("week.jinja")
+    template = jinja_env.get_template(template_name)
 
     return template
+
+def copy_css_to_output():
+    with open("templates/style.css", "r") as f:
+        style_css = f.read()
+    with open("output/html/style.css", "w") as f:
+        f.write(style_css)
+
+def output_as_separate_pages(months):
+    for month_number, month in enumerate(months):
+        for week_number_in_month, page in enumerate(month["weeks"]):
+            print(page)
+
+            # render templates with variables
+            templated_week = week_template.render(days=page, month=month, year = 2026)
+            templated_page = wrapper_template.render(content=templated_week)
+            
+            # define output filename based on month and week number
+            filename = f"page_{month_number+1:03d}_{week_number_in_month+1:03d}"
+
+            # create output directories if they don't exist
+            from pathlib import Path
+            dir = Path("output/html/")
+            dir.mkdir(parents=True, exist_ok=True)
+            dir = Path("output/pdf/")
+            dir.mkdir(parents=True, exist_ok=True)
+
+
+
+            with open(f"output/html/{filename}.html", "w") as f:
+                f.write(templated_page)
+
+            style_path = os.path.join("templates", "style.css")
+            HTML(string=templated_page) \
+            .write_pdf(f"output/pdf/{filename}.pdf", pdf_variant="pdf/x-3", stylesheets=[style_path])
+
+def output_as_single_page(months):
+    all_pages_content = ""
+    for month_number, month in enumerate(months):
+        for week_number_in_month, page in enumerate(month["weeks"]):
+            print(page)
+
+            # render templates with variables
+            templated_week = week_template.render(days=page, month=month, year = 2026)
+            all_pages_content += templated_week
+
+    full_page = wrapper_template.render(content=all_pages_content)
+
+    with open(f"output/html/full_year.html", "w") as f:
+        f.write(full_page)
 
 # init dates
 year_start = dt.date(2026, 1, 1)
@@ -61,22 +111,13 @@ days_cz = ["pondělí", "úterý", "středa", "čtvrtek", "pátek", "sobota", "n
 months_en = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
 months_cz = ["leden", "únor", "březen", "duben", "květen", "červen", "červenec", "srpen", "září", "říjen", "listopad", "prosinec"]
 
+wrapper_template = load_template("wrapper.jinja")
+week_template = load_template("week.jinja")
+# copy style.css to output/html
+copy_css_to_output()
 
 months = generate_data_structure_for_full_year(months_cz, months_en, days_cz, days_en)
-template = load_template("week.jinja")
-for i, month in enumerate(months):
-    for j, page in enumerate(month["weeks"]):
-        print(page)
-        templated_page = template.render(days=page, month=month, year = 2026)
-        filename = f"page_{i+1:03d}_{j+1:03d}"
 
-        from pathlib import Path
-        dir = Path("output/html/")
-        dir.mkdir(parents=True, exist_ok=True)
-        dir = Path("output/pdf/")
-        dir.mkdir(parents=True, exist_ok=True)
 
-        with open(f"output/html/{filename}.html", "w") as f:
-            f.write(templated_page)
-
-        HTML(string=templated_page).write_pdf(f"output/pdf/{filename}.pdf", pdf_variant="pdf/x-3")
+#output_as_separate_pages(months)
+output_as_single_page(months)
